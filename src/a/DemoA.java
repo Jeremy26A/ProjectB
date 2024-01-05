b83464bc920b84376fa18feb360f84b49d6f9d36
JAZUMA <JAZUMA@10.0.0.3>
2024-01-04 18:10:35 -0800

    initial commit

---
+++ src/a/DemoA.java
@@ -0,0 +1,10 @@
+package a;
+
+public class DemoA {
+	public static void main(String[] args) {
+		System.out.println("Monday");
+
+		System.out.println("Day1");
+		System.out.println("Day2");
+	}
+}
